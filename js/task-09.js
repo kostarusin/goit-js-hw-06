00bb4a6fe9a282ba9c58d43f4d034882c7574316
Kostyantyn <kosta.rusin@gmail.com>
2023-05-13 08:13:00 +0200

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const colorButton = document.querySelector('.change-color');
+const bodyElement = document.querySelector('body');
+const colorDescription = document.querySelector('.color');
+
+colorButton.addEventListener('click', () => {
+  bodyElement.style.backgroundColor = getRandomHexColor();
+  colorDescription.textContent = getRandomHexColor();
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
